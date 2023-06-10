3dd3ea199516a7571665452b9f7abe3902d6ba59
chiclaim <chiclaim@gmail.com>
2023-06-10 20:53:18 +0800

    实现我的页面布局

--- lib/module/mine/page/mine_page.dart
+++ lib/module/mine/page/mine_page.dart
@@ -2,6 +2,7 @@
 /// “我的” 页面
 ///
 import 'package:flutter/material.dart';
+import 'package:wanflutter/module/base/constants.dart';
 import 'package:wanflutter/widget/setting_item_widget.dart';
 
 class MinePage extends StatelessWidget {
@@ -10,64 +11,124 @@ class MinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          ListTile(
-            title: const SettingItemWidget(
-                leftIconData: Icons.favorite,
-                text: '我的收藏',
-                rightIconData: Icons.arrow_forward_ios),
-            onTap: () {
-              // 处理我的收藏的点击事件
-            },
-          ),
-          ListTile(
-            title: const SettingItemWidget(
-                leftIconData: Icons.history,
-                text: '历史记录',
-                rightIconData: Icons.arrow_forward_ios),
-            onTap: () {
-              // 处理历史记录的点击事件
-            },
-          ),
-          ListTile(
-            title: const SettingItemWidget(
-                leftIconData: Icons.watch_later,
-                text: '稍后再看',
-                rightIconData: Icons.arrow_forward_ios),
-            onTap: () {
-              // 处理稍后再看的点击事件
-            },
-          ),
-          ListTile(
-            title: const SettingItemWidget(
-                leftIconData: Icons.point_of_sale,
-                text: '我的积分',
-                rightIconData: Icons.arrow_forward_ios),
-            onTap: () {
-              // 处理我的积分的点击事件
-            },
-          ),
-          ListTile(
-            title: const SettingItemWidget(
-                leftIconData: Icons.extension,
-                text: '自定义功能',
-                rightIconData: Icons.arrow_forward_ios),
-            onTap: () {
-              // 处理自定义功能的点击事件
-            },
-          ),
-          ListTile(
-            title: const SettingItemWidget(
-                leftIconData: Icons.logout,
-                text: '退出登录',
-                rightIconData: Icons.arrow_forward_ios),
-            onTap: () {
-              // 处理自定义功能的点击事件
-            },
-          )
-        ],
-      ),
+      appBar: AppBar(
+          title: const Center(
+              child: Text(
+            '我的',
+            style: TextStyle(color: titleColor),
+          )),
+          backgroundColor: mainColor,
+          elevation: 0,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1.0),
+            child: Container(
+              color: const Color(0xFFF2F2F2),
+              height: 1.0,
+            ),
+          )),
+      body: Column(children: [
+        _buildUserWidget(),
+        const SizedBox(height: 30),
+        _buildSettingWidget(),
+      ]),
+    );
+  }
+
+  Widget _buildUserWidget() {
+    return Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 登录入口
+            Container(
+                padding: const EdgeInsets.only(
+                    left: 35, top: 20, right: 20, bottom: 20),
+                child: Row(
+                  children: const [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.grey,
+                      // backgroundImage: AssetImage('assets/avatar.png'),
+                    ),
+                    SizedBox(width: 25),
+                    Text(
+                      '登录/注册',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )),
+            Row(
+              children: [
+                Expanded(
+                    child: Column(children: const [
+                  Text("0",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text('收藏', style: TextStyle(fontSize: 14))
+                ])),
+                Expanded(
+                    child: Column(children: const [
+                  Text("0",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text('积分', style: TextStyle(fontSize: 14))
+                ])),
+                Expanded(
+                    child: Column(children: const [
+                  Text("0",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text('分享', style: TextStyle(fontSize: 14))
+                ])),
+              ],
+            ),
+            const SizedBox(height: 20)
+          ],
+        ));
+  }
+
+  Widget _buildSettingWidget() {
+    return Column(
+      children: [
+        SettingItemWidget(
+          leftIconData: Icons.favorite,
+          text: '我的收藏',
+          rightIconData: Icons.arrow_forward_ios,
+          onTap: () {},
+        ),
+        SettingItemWidget(
+          leftIconData: Icons.history,
+          text: '历史记录',
+          rightIconData: Icons.arrow_forward_ios,
+          onTap: () {},
+        ),
+        SettingItemWidget(
+          leftIconData: Icons.watch_later,
+          text: '稍后再看',
+          rightIconData: Icons.arrow_forward_ios,
+          onTap: () {},
+        ),
+        SettingItemWidget(
+          leftIconData: Icons.point_of_sale,
+          text: '我的积分',
+          rightIconData: Icons.arrow_forward_ios,
+          onTap: () {},
+        ),
+        SettingItemWidget(
+          leftIconData: Icons.extension,
+          text: '自定义功能',
+          rightIconData: Icons.arrow_forward_ios,
+          onTap: () {},
+        ),
+        SettingItemWidget(
+          leftIconData: Icons.logout,
+          text: '退出登录',
+          rightIconData: Icons.arrow_forward_ios,
+          onTap: () {},
+        ),
+      ],
     );
   }
 }
--- lib/widget/setting_item_widget.dart
+++ lib/widget/setting_item_widget.dart
@@ -6,7 +6,8 @@ class SettingItemWidget extends StatelessWidget {
   final String text;
   final TextStyle? textStyle;
   final IconData? rightIconData;
-  static const double defaultIconSize = 20;
+  final GestureTapCallback? onTap;
+  static const double defaultIconSize = 22;
 
   const SettingItemWidget(
       {super.key,
@@ -15,18 +16,28 @@ class SettingItemWidget extends StatelessWidget {
       this.textStyle =
           const TextStyle(fontSize: 16, color: Color(0xFF252933), height: 1.1),
       this.iconTextMargin = 10,
-      this.rightIconData});
+      this.rightIconData,
+      this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        if (leftIconData != null) Icon(leftIconData, size: defaultIconSize),
-        SizedBox(width: iconTextMargin), // 添加间距
-        Text(text, style: textStyle),
-        const Spacer(), // 将箭头图标推到右侧
-        if (rightIconData != null) Icon(rightIconData, size: 16),
-      ],
-    );
+    return Material(
+        color: Colors.white,
+        child: InkWell(
+            onTap: onTap,
+            child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    if (leftIconData != null)
+                      Icon(leftIconData,
+                          color: Colors.grey, size: defaultIconSize),
+                    SizedBox(width: iconTextMargin), // 添加间距
+                    Text(text, style: textStyle),
+                    const Spacer(), // 将箭头图标推到右侧
+                    if (rightIconData != null)
+                      Icon(rightIconData, color: Colors.grey, size: 16),
+                  ],
+                ))));
   }
 }
